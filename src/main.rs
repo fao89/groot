b02375cc7d86492595bf5ecdf3d7fc7b4f34fcbf
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-02-13 15:58:34 -0300

    Download latest collections version

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use async_std::fs::File;
 use async_std::path::Path;
 use async_std::prelude::*;
 use error_chain::error_chain;
+use serde_json::Value;
 use url::Url;
 
 error_chain! {
@@ -14,11 +15,53 @@ error_chain! {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let target =
-        Url::parse("https://galaxy.ansible.com/download/pulp-pulp_installer-3.10.0.tar.gz")?;
+    let content_type = "collections"; // TODO: get it from CLI
+    let root = Url::parse("https://galaxy.ansible.com/api/")?; // TODO: get it from CLI
+    let target = match content_type {
+        "roles" => root.join("v1/roles")?,
+        "collections" => root.join("v2/collections")?,
+        _ => panic!("Invalid content type!"),
+    };
     let response = reqwest::get(target.as_str()).await?;
+    let results = response.json::<Value>().await?;
+    for data in results.as_object().unwrap()["results"].as_array().unwrap() {
+        let content_path = format!(
+            "{}/{}/{}/",
+            content_type,
+            data["namespace"]["name"].as_str().unwrap(),
+            data["name"].as_str().unwrap(),
+        );
+        async_std::fs::create_dir_all(&content_path).await?;
+        download_json(
+            format!("{}metadata.json", content_path).as_str(),
+            data.to_string(),
+        )
+        .await?;
+        let response = reqwest::get(data["latest_version"]["href"].as_str().unwrap()).await?;
+        let json_response = response.json::<Value>().await?;
+        let version_path = format!(
+            "{}/{}/{}/{}/",
+            content_type,
+            json_response["namespace"]["name"].as_str().unwrap(),
+            json_response["collection"]["name"].as_str().unwrap(),
+            json_response["version"].as_str().unwrap(),
+        );
+        async_std::fs::create_dir_all(&version_path).await?;
+        download_json(
+            format!("{}metadata.json", version_path).as_str(),
+            data.to_string(),
+        )
+        .await?;
+        let download_url = Url::parse(json_response["download_url"].as_str().unwrap())?;
+        let response = reqwest::get(download_url.as_str()).await?;
+        let filename = download_url.path_segments().unwrap().last().unwrap();
+        download_tar(format!("{}{}", version_path, filename).as_str(), response).await?
+    }
 
-    let filename = target.path_segments().unwrap().last().unwrap();
+    Ok(())
+}
+
+async fn download_tar(filename: &str, response: reqwest::Response) -> Result<()> {
     println!("Downloading {} ...", filename);
 
     let path = Path::new(filename);
@@ -29,5 +72,16 @@ async fn main() -> Result<()> {
     };
     let content = response.bytes().await?;
     file.write_all(&content).await?;
+    Ok(())
+}
+
+async fn download_json(filename: &str, content: String) -> Result<()> {
+    let path = Path::new(filename);
+
+    let mut file = match File::create(&path).await {
+        Err(why) => panic!("couldn't create {}", why),
+        Ok(file) => file,
+    };
+    file.write_all(&content.as_bytes()).await?;
     Ok(())
 }
