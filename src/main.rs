e7d460e91337803ac97dbce82613f1f12b48c17e
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-02-13 19:24:01 -0300

    Download latest roles version

--- src/main.rs
+++ src/main.rs
@@ -25,39 +25,11 @@ async fn main() -> Result<()> {
     loop {
         let response = reqwest::get(target.as_str()).await?;
         let results = response.json::<Value>().await?;
-        for data in results.as_object().unwrap()["results"].as_array().unwrap() {
-            let content_path = format!(
-                "{}/{}/{}/",
-                content_type,
-                data["namespace"]["name"].as_str().unwrap(),
-                data["name"].as_str().unwrap(),
-            );
-            async_std::fs::create_dir_all(&content_path).await?;
-            download_json(
-                format!("{}metadata.json", content_path).as_str(),
-                data.to_string(),
-            )
-            .await?;
-            let response = reqwest::get(data["latest_version"]["href"].as_str().unwrap()).await?;
-            let json_response = response.json::<Value>().await?;
-            let version_path = format!(
-                "{}/{}/{}/{}/",
-                content_type,
-                json_response["namespace"]["name"].as_str().unwrap(),
-                json_response["collection"]["name"].as_str().unwrap(),
-                json_response["version"].as_str().unwrap(),
-            );
-            async_std::fs::create_dir_all(&version_path).await?;
-            download_json(
-                format!("{}metadata.json", version_path).as_str(),
-                data.to_string(),
-            )
-            .await?;
-            let download_url = Url::parse(json_response["download_url"].as_str().unwrap())?;
-            let response = reqwest::get(download_url.as_str()).await?;
-            let filename = download_url.path_segments().unwrap().last().unwrap();
-            download_tar(format!("{}{}", version_path, filename).as_str(), response).await?
-        }
+        match content_type {
+            "roles" => sync_roles(&results).await?,
+            "collections" => sync_collections(&results).await?,
+            _ => panic!("Invalid content type!"),
+        };
         if results.as_object().unwrap()["next"].as_str().is_none() {
             println!("Sync is complete!");
             break;
@@ -89,5 +61,83 @@ async fn download_json(filename: &str, content: String) -> Result<()> {
         Ok(file) => file,
     };
     file.write_all(&content.as_bytes()).await?;
+    Ok(())
+}
+
+async fn sync_roles(response: &Value) -> Result<()> {
+    for data in response.as_object().unwrap()["results"].as_array().unwrap() {
+        let content_path = format!(
+            "roles/{}/{}/",
+            data["summary_fields"]["namespace"]["name"]
+                .as_str()
+                .unwrap(),
+            data["name"].as_str().unwrap(),
+        );
+        async_std::fs::create_dir_all(&content_path).await?;
+        download_json(
+            format!("{}metadata.json", content_path).as_str(),
+            data.to_string(),
+        )
+        .await?;
+        let version_path = format!(
+            "roles/{}/{}/{}/",
+            data["summary_fields"]["namespace"]["name"]
+                .as_str()
+                .unwrap(),
+            data["name"].as_str().unwrap(),
+            data["github_branch"].as_str().unwrap(),
+        );
+        async_std::fs::create_dir_all(&version_path).await?;
+        download_json(
+            format!("{}metadata.json", version_path).as_str(),
+            data.to_string(),
+        )
+        .await?;
+        let github_url = format!(
+            "https://github.com/{}/{}/archive/{}.tar.gz",
+            data["github_user"].as_str().unwrap(),
+            data["github_repo"].as_str().unwrap(),
+            data["github_branch"].as_str().unwrap()
+        );
+        let download_url = Url::parse(github_url.as_str())?;
+        let response = reqwest::get(download_url.as_str()).await?;
+        let filename = download_url.path_segments().unwrap().last().unwrap();
+        download_tar(format!("{}{}", version_path, filename).as_str(), response).await?
+    }
+    Ok(())
+}
+
+async fn sync_collections(response: &Value) -> Result<()> {
+    for data in response.as_object().unwrap()["results"].as_array().unwrap() {
+        let content_path = format!(
+            "collections/{}/{}/",
+            data["namespace"]["name"].as_str().unwrap(),
+            data["name"].as_str().unwrap(),
+        );
+        async_std::fs::create_dir_all(&content_path).await?;
+        download_json(
+            format!("{}metadata.json", content_path).as_str(),
+            data.to_string(),
+        )
+        .await?;
+        let response = reqwest::get(data["latest_version"]["href"].as_str().unwrap()).await?;
+        let json_response = response.json::<Value>().await?;
+        let version_path = format!(
+            "collections/{}/{}/{}/",
+            json_response["namespace"]["name"].as_str().unwrap(),
+            json_response["collection"]["name"].as_str().unwrap(),
+            json_response["version"].as_str().unwrap(),
+        );
+        async_std::fs::create_dir_all(&version_path).await?;
+        download_json(
+            format!("{}metadata.json", version_path).as_str(),
+            data.to_string(),
+        )
+        .await?;
+        let download_url = Url::parse(json_response["download_url"].as_str().unwrap())?;
+        let response = reqwest::get(download_url.as_str()).await?;
+        let filename = download_url.path_segments().unwrap().last().unwrap();
+        download_tar(format!("{}{}", version_path, filename).as_str(), response).await?
+    }
     Ok(())
 }
