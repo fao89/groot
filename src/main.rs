9d2c29d22a7d57d6b10bcbdb654e535dccabf1ae
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-02-14 12:48:56 -0300

    Download all role versions

--- src/main.rs
+++ src/main.rs
@@ -85,30 +85,32 @@ async fn sync_roles(response: &Value) -> Result<()> {
             data.to_string(),
         )
         .await?;
-        let version_path = format!(
-            "roles/{}/{}/{}/",
-            data["summary_fields"]["namespace"]["name"]
-                .as_str()
-                .unwrap(),
-            data["name"].as_str().unwrap(),
-            data["github_branch"].as_str().unwrap(),
-        );
-        async_std::fs::create_dir_all(&version_path).await?;
-        download_json(
-            format!("{}metadata.json", version_path).as_str(),
-            data.to_string(),
-        )
-        .await?;
-        let github_url = format!(
-            "https://github.com/{}/{}/archive/{}.tar.gz",
-            data["github_user"].as_str().unwrap(),
-            data["github_repo"].as_str().unwrap(),
-            data["github_branch"].as_str().unwrap()
-        );
-        let download_url = Url::parse(github_url.as_str())?;
-        let response = reqwest::get(download_url.as_str()).await?;
-        let filename = download_url.path_segments().unwrap().last().unwrap();
-        download_tar(format!("{}{}", version_path, filename).as_str(), response).await?
+        for version in data["summary_fields"]["versions"].as_array().unwrap() {
+            let version_path = format!(
+                "roles/{}/{}/{}/",
+                data["summary_fields"]["namespace"]["name"]
+                    .as_str()
+                    .unwrap(),
+                data["name"].as_str().unwrap(),
+                version["name"].as_str().unwrap(),
+            );
+            async_std::fs::create_dir_all(&version_path).await?;
+            download_json(
+                format!("{}metadata.json", version_path).as_str(),
+                data.to_string(),
+            )
+            .await?;
+            let github_url = format!(
+                "https://github.com/{}/{}/archive/{}.tar.gz",
+                data["github_user"].as_str().unwrap(),
+                data["github_repo"].as_str().unwrap(),
+                version["name"].as_str().unwrap()
+            );
+            let download_url = Url::parse(github_url.as_str())?;
+            let response = reqwest::get(download_url.as_str()).await?;
+            let filename = download_url.path_segments().unwrap().last().unwrap();
+            download_tar(format!("{}{}", version_path, filename).as_str(), response).await?
+        }
     }
     Ok(())
 }
