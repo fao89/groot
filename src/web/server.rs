162d772c84ba7e5f1aa1d26eddd84f212ef6d26b
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-03-14 16:51:51 -0300

    Initial db setup for collections

--- src/web/server.rs
+++ src/web/server.rs
@@ -1,4 +1,5 @@
 use super::routes::*;
+use crate::db_utils::get_pool;
 use actix_web::{
     middleware::{normalize::TrailingSlash, Logger, NormalizePath},
     rt, App, HttpServer,
@@ -8,6 +9,12 @@ use dotenv::dotenv;
 pub fn start_actix_server() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     pretty_env_logger::init();
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let pool = get_pool(&db_url);
+
+    std::fs::create_dir_all("collections").unwrap();
+    std::fs::create_dir_all("roles").unwrap();
+
     rt::System::new("my actix server").block_on(async move {
         dotenv().ok();
         let config = crate::config::Config::from_env().unwrap();
@@ -15,11 +22,13 @@ pub fn start_actix_server() {
             "Starting server at http://{}:{}",
             config.server.host, config.server.port
         );
-        HttpServer::new(|| {
+        HttpServer::new(move || {
             App::new()
+                .data(pool.clone())
                 .wrap(NormalizePath::new(TrailingSlash::Always))
                 .wrap(Logger::default())
                 .service(api_metadata)
+                .service(start_sync)
                 .service(role_retrieve)
                 .service(role_version_list)
                 .service(collection_retrieve)
