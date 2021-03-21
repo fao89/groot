e9cfae6fbff07db3ae28edac10c9d798e4660e50
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-03-21 17:30:25 -0300

    Retry pg connection

--- src/db_utils/mod.rs
+++ src/db_utils/mod.rs
@@ -3,11 +3,21 @@ use diesel::{
     r2d2::{ConnectionManager, Pool},
     PgConnection,
 };
+use std::time::Duration;
 
 pub fn run_migrations(db_url: &str) {
     embed_migrations!();
-    let connection = PgConnection::establish(db_url).expect("Error connecting to database");
-    embedded_migrations::run_with_output(&connection, &mut std::io::stdout())
+    let mut connection = PgConnection::establish(db_url);
+    for _ in 0..5 {
+        if connection.is_err() {
+            println!("Error connecting to database - Retrying...");
+            std::thread::sleep(Duration::from_secs(30));
+            connection = PgConnection::establish(db_url);
+        } else {
+            break;
+        }
+    }
+    embedded_migrations::run_with_output(&connection.unwrap(), &mut std::io::stdout())
         .expect("Error running migrations");
 }
 
